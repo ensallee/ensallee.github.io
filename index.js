54a03f1bb519a2616cd5d80bd07ef1f4c4ec8954
ensallee <ensallee@gmail.com>
2018-12-23 17:57:37 -0600

    basic email form for desktop and mobile

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   const poetryAnchor = document.getElementById("poetry-anchor");
   const poetryList = document.querySelector(".poetry-list");
+  const contactButton = document.getElementById("contact");
+  const emailForm = document.querySelector(".email-container");
 
-  poetryAnchor.addEventListener("click", toggleVisible);
+  poetryAnchor.addEventListener("click", toggleVisiblePoetry);
+  contactButton.addEventListener("click", toggleVisibleEmail);
 
-  function toggleVisible(e) {
+  function toggleVisiblePoetry(e) {
     e.preventDefault();
     poetryList.classList.toggle("visible");
+  }
+
+  function toggleVisibleEmail(e) {
+    e.preventDefault();
+    emailForm.classList.toggle("visible");
   }
 });
