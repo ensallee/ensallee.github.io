ed714951b50308b7f3d90258e10798979e8d88b8
ensallee <ensallee@gmail.com>
2019-01-24 19:43:44 -0500

    gmail redirect instead of contact form

--- index.js
+++ index.js
@@ -2,22 +2,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const poetryAnchor = document.getElementById("poetry-anchor");
   const poetryList = document.querySelector(".poetry-list");
   const contactButton = document.getElementById("contact");
-  const emailForm = document.querySelector(".email-container");
   const cancelButton = document.getElementById("cancel-button");
-  const autofocus = document.getElementById("autofocus");
 
   poetryAnchor.addEventListener("click", toggleVisiblePoetry);
   contactButton.addEventListener("click", toggleVisibleEmail);
-  cancelButton.addEventListener("click", toggleVisibleEmail)
+  cancelButton.addEventListener("click", toggleVisibleEmail);
 
   function toggleVisiblePoetry(e) {
     e.preventDefault();
     poetryList.classList.toggle("visible");
-  }
-
-  function toggleVisibleEmail(e) {
-    e.preventDefault();
-    emailForm.classList.toggle("visible");
-    autofocus.focus()
   }
 });
