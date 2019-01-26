b85aa08a03c1a899580245a7d4e130567c74a32c
ensallee <ensallee@gmail.com>
2019-01-26 14:46:05 -0500

    event handlers for closing poetry list

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   const poetryAnchor = document.getElementById("poetry-anchor");
   const poetryList = document.querySelector(".poetry-list");
+  const contactAnchor = document.getElementById("contact");
+  const resumeAnchor = document.getElementById("resume");
+  const codeAnchor = document.getElementById("code");
 
   poetryAnchor.addEventListener("click", toggleVisiblePoetry);
+  contactAnchor.addEventListener("click", toggleVisiblePoetry);
+  resumeAnchor.addEventListener("click", toggleVisiblePoetry);
+  codeAnchor.addEventListener("click", toggleVisiblePoetry);
 
   function toggleVisiblePoetry(e) {
-    e.preventDefault();
-    poetryList.classList.toggle("visible");
+    if (e.target.innerHTML === "Poetry") {
+      poetryList.classList.toggle("visible");
+    } else {
+      poetryList.classList.remove("visible");
+    }
   }
 });
