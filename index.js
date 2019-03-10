f6aefe70cb530b5c78f4f8619492a5354db8fcda
ensallee <ensallee@gmail.com>
2019-03-10 12:05:53 -0400

    cleaning up event listeners

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  const poetryAnchor = document.getElementById("poetry-anchor");
+document.addEventListener("DOMContentLoaded", () => {
   const poetryList = document.querySelector(".poetry-list");
-  const contactAnchor = document.getElementById("contact");
-  const resumeAnchor = document.getElementById("resume");
-  const codeAnchor = document.getElementById("code");
+  const body = document.getElementById("root")
 
-  poetryAnchor.addEventListener("click", toggleVisiblePoetry);
-  contactAnchor.addEventListener("click", toggleVisiblePoetry);
-  resumeAnchor.addEventListener("click", toggleVisiblePoetry);
-  codeAnchor.addEventListener("click", toggleVisiblePoetry);
+  body.addEventListener('click', toggleVisiblePoetry)
 
   function toggleVisiblePoetry(e) {
     if (e.target.innerHTML === "Poetry") {
